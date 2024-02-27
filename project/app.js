eeae5d7831bc98eade5d3859f744df8b6efc91c6
blackGhost8278 <161456009+blackGhost8278@users.noreply.github.com>
2024-02-27 12:19:40 +0100

    Add initial project website file

---
+++ project/app.js
@@ -0,0 +1,6 @@
+// document.querySelector('.lau-link').addEventListener('click', onclick);
+var typed = new Typed (".multipule-text", { Strings: ["Promie Olalere", "My frist swies project"], typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
